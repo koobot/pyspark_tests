64e0002f390a76dff0a5fba8a62e5020b9394174
koobot <49636195+koobot@users.noreply.github.com>
2023-12-19 12:12:54 +1100

    added sum zero and null

--- sum_na.py
+++ sum_na.py
@@ -6,10 +6,28 @@ a = spark.createDataFrame(
      ("a", None)],
     ["let", 'num']
 )
-
 a.groupBy("let").agg(F.sum("num")).show()
   # +---+--------+
   # |let|sum(num)|
   # +---+--------+
   # |  a|       3|
   # +---+--------+
+
+b = spark.createDataFrame(
+    [("a", 1),
+     ("a", 2),
+     ("a", None),
+     ("b", 0),
+     ("b", None),
+     ("c", None),
+     ("c", None)],
+    ["let", 'num']
+)
+b.groupBy("let").agg(F.sum("num")).show()
+    # +---+--------+
+    # |let|sum(num)|
+    # +---+--------+
+    # |  b|       0|
+    # |  c|    NULL|
+    # |  a|       3|
+    # +---+--------+
