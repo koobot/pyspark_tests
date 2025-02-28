a6b8c7eb11bb5c5c3157c2718d9cea7638f85b21
koobot <49636195+koobot@users.noreply.github.com>
2025-02-28 12:31:34 +1100

    Update and rename write_csv.py to write_csv_to_s3.py

--- write_csv_to_s3.py
+++ write_csv_to_s3.py
@@ -42,3 +42,5 @@ write_pandas_df_to_s3(
 )
 
 # For pyspark ----------------------------------------------------
+spark.conf.set("fs.s3a.access.key", AWS_ACCESS_KEY_ID)
+spark.conf.set("fs.s3a.secret.key", AWS_SECRET_ACCESS_KEY)
